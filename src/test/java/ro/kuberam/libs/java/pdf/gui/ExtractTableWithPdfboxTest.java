58a3123062deeb154c23f65e8cf92c5b9240d21d
claudius <claudius.teodorescu@gmail.com>
2017-11-22 13:37:53 +0200

    Added new table extraction test.

--- src/test/java/ro/kuberam/libs/java/pdf/gui/ExtractTableWithPdfboxTest.java
+++ src/test/java/ro/kuberam/libs/java/pdf/gui/ExtractTableWithPdfboxTest.java
@@ -1,5 +1,6 @@
 package ro.kuberam.libs.java.pdf.gui;
 
+import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
@@ -8,12 +9,18 @@ import java.util.List;
 
 import javax.xml.stream.XMLStreamException;
 
+import org.apache.pdfbox.pdmodel.PDDocument;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.TestName;
 
 import com.giaybac.traprange.PDFTableExtractor;
 import com.giaybac.traprange.entity.Table;
+
+import technology.tabula.ObjectExtractor;
+import technology.tabula.Page;
+import technology.tabula.extractors.BasicExtractionAlgorithm;
+import technology.tabula.writers.CSVWriter;
 
 public class ExtractTableWithPdfboxTest {
 
@@ -24,18 +31,38 @@ public class ExtractTableWithPdfboxTest {
 	public void traprangeTest1() throws IOException, XMLStreamException {
 		PDFTableExtractor extractor = new PDFTableExtractor();
 
-		List<Table> tables = extractor.setSource("/home/claudius/Downloads/comune.pdf").addPage(48).extract();
+		List<Table> tables = extractor.setSource("/home/claudius/comune.pdf").addPage(5).extract();
 
 		String html = tables.get(0).toHtml();
 
-		try (Writer writer = new OutputStreamWriter(new FileOutputStream("/home/claudius/Downloads/comune.html"),
-				"UTF-8")) {
+		try (Writer writer = new OutputStreamWriter(new FileOutputStream("/home/claudius/comune.html"), "UTF-8")) {
 			for (Table table : tables) {
-				writer.write("Page: " + (table.getPageIdx() + 1) + "\n");
 				writer.write(table.toHtml());
 			}
 		}
 
+	}
+
+	@Test
+	public void tabulaTest1() {
+		ObjectExtractor oe = null;
+		try {
+			PDDocument document = PDDocument.load(new File("/home/claudius/comune.pdf"));
+			oe = new ObjectExtractor(document);
+			Page page = oe.extract(7);
+			oe.close();
+
+			BasicExtractionAlgorithm bea = new BasicExtractionAlgorithm();
+			technology.tabula.Table table = bea.extract(page).get(0);
+
+			StringBuilder sb = new StringBuilder();
+			(new CSVWriter()).write(sb, table);
+			String s = sb.toString();
+			
+			System.out.println(s);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 
 }
